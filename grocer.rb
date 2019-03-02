60b2fac164ac230249e298e7bab9405e4f2efa42
Dennis Chung <chungdennis6@gmail.com>
2019-03-02 03:56:59 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,33 @@ consolidated_cart = {}
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+
+  couponed_item_count = 0
+
+  coupons.each do |coupon_hash|
+    couponed_item = {}
+
+    item_name = coupon_hash[:item]
+
+    if cart[item_name]  && cart[item_name][:count] >= coupon_hash[:num]
+
+      if cart["#{item_name} W/COUPON"]
+
+        cart["#{item_name} W/COUPON"][:count] += 1
+      else
+        couponed_item["#{item_name} W/COUPON"] = {
+                                        price: coupon_hash[:cost],
+                                        clearance: cart[item_name][:clearance],
+                                        count: 1
+                                        }
+        cart["#{item_name} W/COUPON"] = couponed_item["#{item_name} W/COUPON"]                                        
+      end
+
+      cart[item_name][:count] -= coupon_hash[:num]
+    end
+  end
+
+  cart
 end
 
 def apply_clearance(cart)
